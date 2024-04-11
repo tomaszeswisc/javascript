30d9d36322eb245d452c68b5d85c34e368beecab
tomaszeswisc <hiltonelias@senai-pi.com.br>
2024-04-11 10:50:55 -0300

    CondicionalM

---
+++ condicionais/JS/exercicio.js
@@ -0,0 +1,4 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+console.log('Dirija sempre usando o cinto de segurança')
+console.log('Você ultrapassou a velocidade permitida, MULTADO!')
---
+++ condicionais/JS/exercicio2.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro!') //True
+}
+else {
+    console.log('Você é Estrangeiro!') //False
+}
