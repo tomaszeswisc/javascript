3be9901c4af37a95882e7b5b716944281c64f867
tomaszeswisc <hiltonelias@senai-pi.com.br>
2024-04-12 10:51:20 -0300

    Condicional

---
+++ condicionais/JS/aninhada/exercicio1.js
@@ -0,0 +1,6 @@
+var idade = 12
+if (idade < 16){
+    console.log('Não Vota!')
+} else {
+    console.log('Vota!')
+}
---
+++ condicionais/JS/aninhada/exercicio1_1.js
@@ -0,0 +1,9 @@
+var idade = 18
+if (idade < 16) {
+    console.log('Não vota!')
+} else {
+    if (idade >= 16 && idade < 18) {
+        console.log('Voto opcional')
+    }
+    
+}
